a1ac2a52bd89b1b3fec0e33c07882b1cf433bd5d
kishanV2321 <kv230801@gmail.com>
2024-09-30 13:17:20 +0530

    email js

--- src/app/contact/ContactForm.tsx
+++ src/app/contact/ContactForm.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Input from "./Input";
 import { useForm, SubmitHandler } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 interface ContactFormData {
-    firstname: string;
-    lastname: string;
+    fullname: string;
     email: string;
-    phoneNo: string;
+    phonenumber: string;
     message: string;
-    countrycode: string;
 }
 
 function ContactForm() {
     const [loading, setLoading] = useState(false);
+    const formRef = useRef<HTMLFormElement | null>(null);
     const {
         register,
         handleSubmit,
@@ -20,66 +20,70 @@ function ContactForm() {
         formState: { errors, isSubmitSuccessful },
     } = useForm<ContactFormData>();
 
+
     const submitContactForm: SubmitHandler<ContactFormData> = async (data) => {
         try {
             setLoading(true);
-            // Perform API call here
-            console.log("Form Data - ", data);
+
+            const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID as string;
+            const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID as string;
+            const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string;
+
+
+            if (formRef.current) {
+                await emailjs.sendForm(
+                    serviceId,
+                    templateId,
+                    formRef.current,
+                    publicKey
+                );
+                console.log("Form Data - ", data);
+            } else {
+                throw new Error("Form reference is null");
+            }
             setLoading(false);
         } catch (error: any) {
-            console.log("ERROR MESSAGE - ", error.message);
+            console.log("ERROR OBJECT - ", error);
+            if (error.text) {
+                console.log("ERROR TEXT - ", error.text);
+            }
             setLoading(false);
         }
     };
+
 
     useEffect(() => {
         if (isSubmitSuccessful) {
             reset({
                 email: "",
-                firstname: "",
-                lastname: "",
+                fullname: "",
                 message: "",
-                phoneNo: "",
+                phonenumber: "",
             });
         }
     }, [reset, isSubmitSuccessful]);
 
     return (
         <form
+            ref={formRef}
             className="flex flex-col gap-7"
             onSubmit={handleSubmit(submitContactForm)}
         >
-            <div className="flex flex-col gap-5 lg:flex-row">
-                <div className="flex flex-col gap-2 lg:w-[48%]">
-                    <Input
-                        label="First Name"
-                        type="text"
-                        id="firstname"
-                        placeholder="Enter first name"
-                        className="bg-[#2c333f]"
-                        {...register("firstname", { required: true })}
-                    />
-                    {errors.firstname && (
-                        <span className="-mt-1 text-[12px] text-yellow-100">
-                            Please enter your first name.
-                        </span>
-                    )}
-                </div>
-                <div className="flex flex-col gap-2 lg:w-[48%]">
-                    <Input
-                        label="Last Name"
-                        type="text"
-                        id="lastname"
-                        placeholder="Enter last name"
-                        className="bg-[#2c333f]"
-                        {...register("lastname", { required: true })}
-                    />
-                    {errors.lastname && (
-                        <span className="-mt-1 text-[12px] text-yellow-100">
-                            Please enter your last name.
-                        </span>
-                    )}
-                </div>
+
+            <div className="flex flex-col gap-2">
+                <Input
+                    label="Full Name"
+                    type="text"
+                    id="fulltname"
+                    placeholder="Enter first name"
+                    className="bg-[#2c333f]"
+                    {...register("fullname", { required: true })}
+                />
+                {errors.fullname && (
+                    <span className="-mt-1 text-[12px] text-yellow-100">
+                        Please enter your first name.
+                    </span>
+                )}
             </div>
 
             <div className="flex flex-col gap-2">
@@ -105,7 +109,7 @@ function ContactForm() {
                     id="phonenumber"
                     placeholder="12345 67890"
                     className="bg-[#2c333f]"
-                    {...register("phoneNo", {
+                    {...register("phonenumber", {
                         required: {
                             value: true,
                             message: "Please enter your Phone Number.",
@@ -114,9 +118,9 @@ function ContactForm() {
                         minLength: { value: 10, message: "Invalid Phone Number" },
                     })}
                 />
-                {errors.phoneNo && (
+                {errors.phonenumber && (
                     <span className="-mt-1 text-[12px] text-yellow-100">
-                        {errors.phoneNo.message}
+                        {errors.phonenumber.message}
                     </span>
                 )}
             </div>
@@ -124,7 +128,7 @@ function ContactForm() {
             <div className="flex flex-col gap-2">
                 <label
                     htmlFor="message"
-                    className="lable-style flex items-center gap-1"
+                    className="flex items-center gap-1"
                 >
                     Message
                     <sup className="text-pink-200">*</sup>
@@ -134,7 +138,7 @@ function ContactForm() {
                     cols={30}
                     rows={7}
                     placeholder="Enter your message here"
-                    className="bg-[#2c333f]"
+                    className="bg-[#2c333f] p-4 rounded-lg"
                     {...register("message", { required: true })}
                 />
                 {errors.message && (
@@ -152,7 +156,7 @@ function ContactForm() {
                     "transition-all duration-200 hover:scale-95 hover:shadow-none"
                     }  disabled:bg-richblack-500 sm:text-[16px] `}
             >
-                Send Message
+                {loading ? "Sending..." : "Send Message"}
             </button>
         </form>
     );
